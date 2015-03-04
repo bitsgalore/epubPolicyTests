9ec68290be1194d5240ca03323d494487f45198d
bitsgalore <johan.vanderknijff@kb.nl>
2015-03-04 16:32:36 +0100

    script now works, added fake encrypted example

--- buildepubs.sh
+++ buildepubs.sh
@@ -2,6 +2,9 @@
 
 # Build Epubs from directory structure
 
+# Working dir - directory from which script is executed
+workDir="$(pwd)"
+
 # Content directory - every subdir here represents one epub
 contentDir="./content/"
 
@@ -21,16 +24,22 @@ while IFS= read -d $'\0' directory ; do
     # Generate name for output file, based on directory name
     epubName=$buildDir"$dirName".epub
 
-    echo $epubName
+    # Convert to absolute path
+    epubNameAbs=$(readlink -f $epubName)
+
+    echo $epubNameAbs
 
     # Go to dir with content
     cd "$directory"
 
     # Create ZIP file and add mimetype resource without compressing
-    zip -X0 "$epubName" mimetype
+    zip -X0 "$epubNameAbs" mimetype
 
     # Add remaining files with compression
-    zip -rDX9 "$epubName" * -x mimetype
+    zip -rDX9 "$epubNameAbs" * -x mimetype
+
+    # Back to working dir
+    cd "$workDir"
       
-done < <(find "$contentDir" -type d -maxdepth 1 -mindepth 1 -print0)
+done < <(find "$contentDir" -maxdepth 1 -mindepth 1 -print0 -type d)
 
